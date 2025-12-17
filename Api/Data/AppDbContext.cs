cd1782baabcd7214352b2bc3fd7970201546be60
Şerif ERGİN <serifergin48@gmail.com>
2025-12-17 12:49:33 +0300

    changed entities names

--- Api/Data/AppDbContext.cs
+++ Api/Data/AppDbContext.cs
@@ -11,4 +11,18 @@ public class AppDbContext : DbContext
     }
 
     public DbSet<User> Users => Set<User>();
+    public DbSet<Project> Projects => Set<Project>();
+    public DbSet<TaskItem> TaskItems => Set<TaskItem>();
+    public DbSet<Comment> Comments => Set<Comment>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Soft Delete
+        modelBuilder.Entity<User>().HasQueryFilter(x => !x.IsDeleted);
+        modelBuilder.Entity<Project>().HasQueryFilter(x => !x.IsDeleted);
+        modelBuilder.Entity<TaskItem>().HasQueryFilter(x => !x.IsDeleted);
+        modelBuilder.Entity<Comment>().HasQueryFilter(x => !x.IsDeleted);
+    }
 }
